9b6020014c739af57e9a1e1ce760da0be8771919
Salman Ahmed <salman@Salmans-Macbook-Pro-2.local>
2020-04-07 16:07:43 +0530

    added more data

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import { createStore } from 'redux';
 // import StudentList from './screens/studentlist';
 // import UserList from './screens/users';
 import CovidData from './screens/coviddata';
+import { Navbar, Carousel } from 'react-bootstrap';
+
 
 class App extends React.Component{
   constructor(props){
@@ -22,7 +24,46 @@ class App extends React.Component{
   render(){
     return(
       <Provider store={createStore(reducers)}>
-        <div>
+        
+        <Navbar style={{backgroundColor:"#515151"}} variant="dark">
+                <Navbar.Brand href='/'> 
+                  <h3> Covid-19  </h3>
+                  <span> live data - by Salman Ahmed </span> 
+                </Navbar.Brand>
+            </Navbar>
+            <Carousel>
+              <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="./assets/slide1.jpeg"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Live Updates</h3>
+                <p>#stayhome #workfromhome</p>
+              </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./assets/wash.jpg"
+              alt="second slide"
+            /> 
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./assets/slide_3.png"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+        <div style={{flexDirection:'row', justifyContent:'center', display:'flex', marginTop:'10px'}}>
+          <h1 style={{color:'#515151'}}>
+            Stay home Stay Safe
+          </h1>
+        </div>
+        <div style={{flexDirection:'row', justifyContent:'center', display:'flex'}}>
           <CovidData />
         </div>
       </Provider>
--- src/screens/coviddata.js
+++ src/screens/coviddata.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import { Card, CardGroup } from 'react-bootstrap';
+
 
 
 class CovidData extends React.Component{
@@ -7,7 +9,6 @@ class CovidData extends React.Component{
         super(props);
         this.state = {
             countries : [],
-            info : []
         }
     }
 
@@ -24,28 +25,38 @@ class CovidData extends React.Component{
         })
     }
 
-    // allFlags() {
-    //     this.state.
-    // }
-
     allCovidData(){
         return this.state.countries.map((item, index) => (
-            <div key={index}>
-                 <div>
-                        <img src={Object.values(item.countryInfo)[5]} alt='flag'>
-                    </img>
-                <h1>
-                    {item.country}
-                </h1>
-                <h1>
-                   cases :  {item.cases}
-                </h1>
-               <h2>
-                todayCases : {item.todayCases}
-                </h2>
-                {/* <h4>
-                   flag : 
-                   {Object.keys(item.countryInfo).map((keyName, i) => (
+            <div key={index}> 
+                {/* <div>
+                    <h1>
+                        {item.country}
+                    </h1>
+                    <img src={Object.values(item.countryInfo)[5]} alt='flag' />
+                    <h6>
+                        Cases :  {item.cases}
+                    </h6>
+                    <h6>
+                        Today Cases : {item.todayCases}
+                    </h6>
+                    <h6>
+                        Deaths : {item.deaths}
+                    </h6>
+                    <h6>
+                        Today Deaths : {item.todayDeaths}   
+                    </h6>
+                    <h6>
+                        Recovered : {item.recovered}    
+                    </h6>
+                    <h6>
+                        Active : {item.active} 
+                    </h6>
+                    <h6>
+                        Critical : {item.critical}
+                    </h6>                
+                </div> */}
+
+                {/* {Object.keys(item.countryInfo).map((keyName, i) => (
                         <div key={i}>
                         
                             <img alt='flag' src={item.countryInfo[keyName]} >
@@ -53,33 +64,48 @@ class CovidData extends React.Component{
                             </img>
                         </div>
                 ))}
-                </h4> */}
+                 */}
 
-                {/* countryInfo:
-                        _id: 840
-                        iso2: "US"
-                        iso3: "USA"
-                        lat: 38
-                        long: -97
-                        flag: " */}
-
-                   
-
-                   </div>
-                        
-
+                <CardGroup style={{paddingTop:20, paddingLeft:10, paddingRight:10, alignContent: 'center'}}>
+                    <Card border="dark" style={{paddingLeft:20, paddingRight:20, paddingTop:20, width: '18rem' }}>
+                        <Card.Img variant="top" style={{width: 200, height: 200}} src={Object.values(item.countryInfo)[5]} alt='flag' />
+                        <Card.Body>
+                            <Card.Title style={{ flexWrap: "wrap", width: 200}} > <b> {item.country} </b> </Card.Title>
+                                <Card.Text>
+                                <h6 style={{color:'blue'}}>
+                                    Cases :  {item.cases}
+                                </h6>
+                                <h6>
+                                    Today Cases : {item.todayCases}
+                                </h6>
+                                <h6 style={{color:'red'}}>
+                                    Deaths : {item.deaths}
+                                </h6>
+                                <h6>
+                                    Today Deaths : {item.todayDeaths}   
+                                </h6>
+                                <h6 style={{color:'green'}}>
+                                    Recovered : {item.recovered}    
+                                </h6>
+                                <h6>
+                                    Active : {item.active} 
+                                </h6>
+                                <h6>
+                                    Critical : {item.critical}
+                                </h6>   
+                                    
+                                </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
             </div>
         ))
     }
 
-
-
-    
-
     render(){
-        console.log('flag info', this.state.info)
         return(
-            <div>
+            <div style={{display: 'flex',justifyContent:'center',flexDirection:'row', flexWrap: "wrap"}}>   
+
                 {this.allCovidData()}
             </div>
         )
